a095515e580e84e14f61bdd318caafbf4c2e028f
ssloy <spam@skisa.org>
2018-06-04 14:04:58 +0200

    params

--- params.h
+++ params.h
@@ -1,19 +1,18 @@
-// conservative settings (for white noise)
-#define PRESET 1 
+#define PRESET 0
 
-#if 0==PRESET
+#if 0==PRESET               // conservative settings (white noise)
 #define VORO_BLOCK_SIZE 16
 #define KNN_BLOCK_SIZE  32
 #define _K_             190
 #define _MAX_P_         64
 #define _MAX_T_         96
-#elif 1==PRESET
+#elif 1==PRESET            // perturbed grid settings
 #define VORO_BLOCK_SIZE 16
 #define KNN_BLOCK_SIZE  32
 #define _K_             90
 #define _MAX_P_         50
 #define _MAX_T_         96
-#elif 2==PRESET
+#elif 2==PRESET            // blue noise settings
 #define VORO_BLOCK_SIZE 16
 #define KNN_BLOCK_SIZE  64
 #define _K_             35
